618fe10ad61f010ef5f95d652fe6e6a769bdec8c
cmkxak <gcm466@naver.com>
2022-12-20 21:51:25 +0900

    add : [#4] JOIN DTO 설계 1. UserJoinRequest 2. UserJoinResponse 3. ResponseDTO 추상화

---
+++ src/main/java/com/likelion/mutsasns/domain/dto/user/UserJoinResponse.java
@@ -0,0 +1,11 @@
+package com.likelion.mutsasns.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserJoinResponse {
+    private String userId;
+    private String userName;
+}
