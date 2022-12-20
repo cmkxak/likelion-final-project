430c0a5e7d5e48fdf42026bded7f554dda0758b8
cmkxak <gcm466@naver.com>
2022-12-20 11:09:44 +0900

    add : build Project

---
+++ src/main/java/com/likelion/mutsasns/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.likelion.mutsasns.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
