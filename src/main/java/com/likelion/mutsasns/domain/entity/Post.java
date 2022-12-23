40bc1ab63218a92d5717b96e38f03d7f4566a1f9
cmkxak <gcm466@naver.com>
2022-12-23 14:00:27 +0900

    refactor : [#9] super 키워드 추가

--- src/main/java/com/likelion/mutsasns/domain/entity/Post.java
+++ src/main/java/com/likelion/mutsasns/domain/entity/Post.java
@@ -33,8 +33,8 @@ public class Post extends BaseEntity{
                 .title(title)
                 .body(body)
                 .userName(this.user.getUserName())
-                .createdAt(getCreatedAt())
-                .lastModifiedAt(getLastModifiedAt())
+                .createdAt(super.getCreatedAt())
+                .lastModifiedAt(super.getLastModifiedAt())
                 .build();
     }
 }
