90a6ef387fbaa8f1b841368dc0568298c28f1032
cmkxak <gcm466@naver.com>
2022-12-23 13:33:57 +0900

    refactor : 불필요한 로직 제거

--- src/main/java/com/likelion/mutsasns/exception/AppException.java
+++ src/main/java/com/likelion/mutsasns/exception/AppException.java
@@ -1,14 +1,11 @@
 package com.likelion.mutsasns.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class AppException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
-
-    public AppException(ErrorCode errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message + "는 이미 있습니다.";
-    }
 }
