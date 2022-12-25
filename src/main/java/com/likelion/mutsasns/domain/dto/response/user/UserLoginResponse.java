3cf179476b9975ed123aed79a7eac5602cf088e8
cmkxak <gcm466@naver.com>
2022-12-25 23:30:10 +0900

    refactor : [#7] 로그인 성공 테스트 코드 1. 요구사항에 맞게 login() 메소드 실행 시, token이 리턴 되도록 수정

--- src/main/java/com/likelion/mutsasns/domain/dto/response/user/UserLoginResponse.java
+++ src/main/java/com/likelion/mutsasns/domain/dto/response/user/UserLoginResponse.java
@@ -2,8 +2,10 @@ package com.likelion.mutsasns.domain.dto.response.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class UserLoginResponse {
     private String jwt;
